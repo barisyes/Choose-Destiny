740fd67873f13350356a15fcd0fa38faea812669
barisyes <barisyesilkayaa@gmail.com>
2022-07-03 15:22:25 +0300

    Initial Commit

---
+++ Choose Destiny/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Choose Destiny
+//
+//  Created by Barış Yeşilkaya on 3.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
